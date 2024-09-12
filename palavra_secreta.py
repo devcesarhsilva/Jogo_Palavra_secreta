51e81a6434f865bba90ef97ce51ea27a4c5a5ecb
devcesar <dev.cesar.hsilva@gmail.com>
2024-09-12 12:38:13 -0300

    first

---
+++ palavra_secreta.py
@@ -0,0 +1,24 @@
+palavra_secreta = 'papibaquigrafo'
+letras_acertadas = ''
+while True:
+    letra_digitada = input('Digite uma letra: ')
+
+    if len(letra_digitada) > 1:
+        print('Digite apenas uma letra.')
+        continue
+ 
+    if letra_digitada in palavra_secreta:
+        letras_acertadas += letra_digitada
+
+    palavra_formada = ''
+    for letra_secreta in palavra_secreta:
+        if letra_secreta in letras_acertadas:
+            palavra_formada += letra_secreta
+        else:
+            palavra_formada += '*'
+    print('Palavra formada', palavra_formada)
+            
+
+    if palavra_formada == palavra_secreta:
+        print('PARABÉNS, VOCÊ VENCEU')
+        print('A palavra secreta era: ', palavra_secreta)
